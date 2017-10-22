3899c965f8d1c3cdabf4478a32ddb1e844cd3daf
Ygenks <kolxozniko@gmail.com>
2017-10-22 23:06:15 +0300

    First and second labs

---
+++ 1/main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import math
+import click
+import matplotlib.pyplot as plt
+from scipy import stats
+
+N = 15_000_0
+
+
+def lcg(a, modulus, seed):
+    while True:
+        seed = a * seed % modulus
+        yield seed / modulus
+
+
+@click.command()
+@click.option('-a', default=134775813, help='a coefficient')
+@click.option('-m', default=2**32 - 1, help='modulus')
+@click.option('-s', default=12345, help='seed')
+def main(a, m, s):
+
+    rand = lcg(a, m, s)
+
+    numbers = np.fromiter(rand, dtype=np.float32, count=N)
+
+    print('A: {}'.format(a))
+    print('Modulus: {}'.format(m))
+    print('Seed: {}'.format(s))
+    print('Mean: {}'.format(np.mean(numbers)))
+    print('Variance: {}'.format(np.var(numbers, ddof=1)))
+    print('Std: {}'.format(np.std(numbers, ddof=1)))
+
+    pairs = zip(numbers[::2], numbers[1::2])
+
+    K = 0
+    for pair in pairs:
+        x1, x2 = pair
+        if x1 * x1 + x2 * x2 < 1:
+            K += 1
+
+    print('Indirect assessment: {}'.format((2 * K) / N - np.pi / 4))
+
+    xv = numbers[-1]
+    index1 = index2 = -1
+    for i in range(N):
+        if math.isclose(numbers[i], xv):
+            if index1 == -1:
+                index1 = i
+            elif index2 == -1:
+                index2 = i
+                break
+
+    period = (index2 if index2 != -1 else N - 1) - index1
+    print('Period: {}'.format(period))
+
+    index1 = index2 = 0
+
+    for i in range(N - period):
+        if math.isclose(numbers[i], numbers[i + period]):
+            index1 = i
+            break
+
+    for i in range(period, N - period):
+        if math.isclose(numbers[i], numbers[i + period]):
+            index2 = i
+            break
+
+    aperiodic = min(index1, index2) + period
+
+    print('La: {}'.format(aperiodic))
+
+    plt.hist(numbers, bins=20, normed=True, edgecolor='w')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2/main.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import click
+
+N = 10000
+R = np.random.uniform(0.0, 1.0, N)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.resultcallback()
+def draw_plot(*args):
+
+    print('Mean: {}'.format(np.mean(args)))
+    print('Std: {}'.format(np.std(args)))
+    print('Variance: {}'.format(np.std(args)**2))
+
+    plt.hist(args, bins=50)
+    plt.show()
+
+
+@cli.command(help='Generate uniform distribution')
+@click.argument('a', required=True, type=click.FLOAT, metavar='<left margin>')
+@click.argument('b', required=True, type=click.FLOAT, metavar='<right margin>')
+def uniform(a, b):
+
+    uni = a + (b - a) * R
+
+    return uni
+
+
+@cli.command(help='Generate normal distribution')
+@click.argument('mean', required=True, type=click.FLOAT)
+@click.argument('std', required=True, type=click.FLOAT)
+def norm(mean, std):
+
+    normed = np.empty_like(R)
+    for i in range(N):
+        normed[i] = mean + std * np.sqrt(2) * (sum(np.random.choice(R, 6)) - 3)
+
+    return normed
+
+
+@cli.command(help='Generate exponential distribution')
+@click.argument('l', required=True, type=click.FLOAT)
+def exponential(l):
+
+    exp = -1 / l * np.log(R)
+
+    return exp
+
+
+@cli.command(help='Generate erlang distribution')
+@click.argument('eta', required=True, type=click.INT)
+@click.argument('l', required=True, type=click.FLOAT)
+def erlang(eta, l):
+
+    gamma = np.empty_like(R)
+    for i in range(N):
+        gamma[i] = (-1 / l) * np.log(np.prod(np.random.choice(R, eta)))
+
+    return gamma
+
+
+@cli.command(help='Generate triangular distribution')
+@click.argument('a', required=True, type=click.FLOAT)
+@click.argument('b', required=True, type=click.FLOAT)
+@click.argument('pdf', required=True, default='down')
+def triangular(a, b, pdf):
+
+    f = max if pdf == 'up' else min
+
+    triang = np.empty_like(R)
+    for i in range(N):
+        triang[i] = a + (b - a) * f(np.random.choice(R, 2))
+
+    return triang
+
+
+@cli.command(help='Generate simpson distribution')
+@click.argument('a', required=True, type=click.FLOAT)
+@click.argument('b', required=True, type=click.FLOAT)
+def simpson(a, b):
+
+    simp = np.empty_like(R)
+    for i in range(N):
+        simp[i] = sum(np.random.uniform(a / 2, b / 2, 2))
+
+    return simp
+
+
+if __name__ == '__main__':
+    cli()
